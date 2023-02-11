6351e21391afcb6a36965d33433bb1a5a8240db0
udoy2 <udoyrahman983@gmail.com>
2023-02-11 21:25:46 +0600

    About us page added

---
+++ src/Aboutus/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import "./about.css";
+function About() {
+  return (
+    <>
+        <Navbar />
+        <main>
+            main
+        </main>
+        <Footer />
+    </>
+  )
+}
+
+export default About
